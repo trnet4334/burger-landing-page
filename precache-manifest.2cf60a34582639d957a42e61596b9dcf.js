f76ce2d2c0f56a2a3d4b90504ce048301c57993a
trnet4334 <search0210@gmail.com>
2020-10-22 22:26:56 +0800

    Deploy

---
+++ precache-manifest.2cf60a34582639d957a42e61596b9dcf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2d29db41126cd8556e",
+    "url": "/burger-landing-page/css/app.225c3ad4.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/burger-landing-page/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/burger-landing-page/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "fbd214bc875e403292ef28d455b5efe7",
+    "url": "/burger-landing-page/img/about-image-1.fbd214bc.jpg"
+  },
+  {
+    "revision": "127ddd09142dceeb40058b381ab765bc",
+    "url": "/burger-landing-page/img/about-image-2.127ddd09.jpg"
+  },
+  {
+    "revision": "c5f5735d852ea2dca17f9b73efd68883",
+    "url": "/burger-landing-page/img/feed-image-1.c5f5735d.jpg"
+  },
+  {
+    "revision": "5aee03345438d2c0794af7d09d33479c",
+    "url": "/burger-landing-page/img/feed-image-2.5aee0334.jpg"
+  },
+  {
+    "revision": "aa217f8bf62254f68a7bdd362234aef5",
+    "url": "/burger-landing-page/img/feed-image-3.aa217f8b.jpg"
+  },
+  {
+    "revision": "5fd2e322d5df7b9a3ca9db155d9f3c86",
+    "url": "/burger-landing-page/img/feed-image-4.5fd2e322.jpg"
+  },
+  {
+    "revision": "8bc1d9e715b586300fc9f05062141072",
+    "url": "/burger-landing-page/img/feed-image-5.8bc1d9e7.jpg"
+  },
+  {
+    "revision": "580ee6135b159e2503fc58cbdbe40f29",
+    "url": "/burger-landing-page/img/feed-image-6.580ee613.jpg"
+  },
+  {
+    "revision": "483ae3342913bf725843238c3e650d71",
+    "url": "/burger-landing-page/img/feed-image-7.483ae334.jpg"
+  },
+  {
+    "revision": "8b47858b62e234ee7220a499b489305c",
+    "url": "/burger-landing-page/img/feed-image-8.8b47858b.jpg"
+  },
+  {
+    "revision": "f258cb1da95a17b613adc68be3db3364",
+    "url": "/burger-landing-page/img/feed-image-9.f258cb1d.jpg"
+  },
+  {
+    "revision": "3634ddee5f3c8991a378e0c5aacb2299",
+    "url": "/burger-landing-page/img/home-iamge-2.3634ddee.jpg"
+  },
+  {
+    "revision": "e2590ce750a32b4d1fcefa90e67e2707",
+    "url": "/burger-landing-page/img/home-image-1.e2590ce7.jpg"
+  },
+  {
+    "revision": "6af7d3c4684071006310692d144376b3",
+    "url": "/burger-landing-page/img/logo.6af7d3c4.png"
+  },
+  {
+    "revision": "e1de24b8aff9efed8ecbc6507aca0239",
+    "url": "/burger-landing-page/img/menu-image-1.e1de24b8.jpg"
+  },
+  {
+    "revision": "2f1e186d317bf4fc7bd0754eb3e4519e",
+    "url": "/burger-landing-page/img/menu-image-2.2f1e186d.jpg"
+  },
+  {
+    "revision": "27314c11a50673e20cd38fce29956f65",
+    "url": "/burger-landing-page/index.html"
+  },
+  {
+    "revision": "0f2d29db41126cd8556e",
+    "url": "/burger-landing-page/js/app.1b37d7e2.js"
+  },
+  {
+    "revision": "de81faea3d7609a3ae0d",
+    "url": "/burger-landing-page/js/chunk-vendors.6eee432c.js"
+  },
+  {
+    "revision": "f4d11fc4277b27e81e91811be73fa766",
+    "url": "/burger-landing-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/burger-landing-page/robots.txt"
+  }
+]);
